5e09117bc9c12b55e96d3f9a33bbc9d025d0ebb1
MaganaAsiati <assia.magana@gmail.com>
2022-09-08 00:40:54 +0200

    fix linters

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,8 +2,6 @@ class Category < ApplicationRecord
   has_many :deals
   belongs_to :user, class_name: 'User', foreign_key: :user_id
 
-
-
   validates :name, length: { minimum: 1 }
   validates :icon, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,5 @@ class User < ApplicationRecord
   has_many :categories, dependent: :destroy
   has_many :deals, dependent: :destroy
 
-
   validates :name, length: { minimum: 1 }
 end
