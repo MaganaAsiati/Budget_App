eea44428ae556109ba79dac57509effa2c7ff598
MaganaAsiati <assia.magana@gmail.com>
2022-09-07 13:19:25 +0200

    add validation for deal

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,4 +1,8 @@
 class Deal < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: :author_id
   belongs_to :category, class_name: 'Category', foreign_key: :category_id
+
+  validates :name, length: { minimum: 1 }
+  validates :amount, numericality: { only_integer: true, greater_than: 0 }
+
 end
