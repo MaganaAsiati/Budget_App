7827d1d2e90e486431b23e0e12261c13b8fea866
MaganaAsiati <assia.magana@gmail.com>
2022-09-07 11:52:18 +0200

    configirate route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,13 @@ Rails.application.routes.draw do
   resources :categories
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :categories, only: [:index, :show, :new, :create] do
+      resources :deals, only: [:index, :show, :new, :create]
+    end
+  end
 
+  # Defines 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#splash_screen"
 end
