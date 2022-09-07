f7e930514bc441a37e7f8450d5e7d45dcdb68809
MaganaAsiati <assia.magana@gmail.com>
2022-09-08 00:43:47 +0200

    fix linters

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,6 @@ class Category < ApplicationRecord
   has_many :deals
   belongs_to :user, class_name: 'User', foreign_key: :author_id
 
-
   validates :name, length: { minimum: 1 }
   validates :icon, presence: true
 end
