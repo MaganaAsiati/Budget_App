87495e9b27085b508ad744df05d4d99f4db07f8e
MaganaAsiati <assia.magana@gmail.com>
2022-09-08 00:17:12 +0200

    update deals controller

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -3,7 +3,8 @@ class DealsController < ApplicationController
 
   # GET /deals or /deals.json
   def index
-    @deals = Deal.all
+    @deals = Deal.where(category_id: params[:category_id]).order(created_at: :desc)
+    @category = Category.find(params[:category_id])
   end
 
   # GET /deals/1 or /deals/1.json
@@ -12,6 +13,8 @@ class DealsController < ApplicationController
   # GET /deals/new
   def new
     @deal = Deal.new
+    @category = Category.find(params[:category_id])
+    p @category
   end
 
   # GET /deals/1/edit
@@ -19,11 +22,14 @@ class DealsController < ApplicationController
 
   # POST /deals or /deals.json
   def create
-    @deal = Deal.new(deal_params)
+    @user = current_user
+    @category = Category.find(params[:category_id])
+    @deal = Deal.create(user: current_user, category_id: @category.id, name: deal_params['name'],
+                        amount: deal_params['amount'])
 
     respond_to do |format|
       if @deal.save
-        format.html { redirect_to deal_url(@deal), notice: 'Deal was successfully created.' }
+        format.html { redirect_to category_deals_path(@category) }
         format.json { render :show, status: :created, location: @deal }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -50,7 +56,7 @@ class DealsController < ApplicationController
     @deal.destroy
 
     respond_to do |format|
-      format.html { redirect_to deals_url, notice: 'Deal was successfully destroyed.' }
+      format.html { redirect_to category_deals_path, notice: 'Deal was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -64,6 +70,6 @@ class DealsController < ApplicationController
 
   # Only allow a list of trusted parameters through.
   def deal_params
-    params.require(:deal).permit(:name, :amount, :user_id, :category_id)
+    params.require(:deal).permit(:name, :amount)
   end
 end
